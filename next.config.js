def8b9c9e6752214611920343401d3da84302a24
mcesalt <mcesalt@users.noreply.github.com>
2026-06-12 20:29:46 -0700

    Fix GitHub Pages deploy config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+const repo = "srch"
+const isGithubActions = process.env.GITHUB_ACTIONS === "true"
+
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  output: "export",
+  basePath: isGithubActions ? `/${repo}` : "",
+  assetPrefix: isGithubActions ? `/${repo}/` : "",
+  images: {
+    unoptimized: true,
+  },
 }
- 
+
 module.exports = nextConfig
